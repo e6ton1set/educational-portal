2f755ec32674039112ea5a52f54c917305015e3e
Ruslan Karimov <ru-ka-@mail.ru>
2024-02-06 11:35:49 +0500

    added distribution of media files by the built-in development server

--- base/settings.py
+++ base/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Раздача медиафайлов
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
