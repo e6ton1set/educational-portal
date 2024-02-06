e6aeba230c3f1ecfb9400236adf7b63b729f1287
Ruslan Karimov <ru-ka-@mail.ru>
2024-02-06 16:16:27 +0500

    registration of course models in the admin panel

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from courses.models import Program, Course, Module
 
-# Register your models here.
+
+@admin.register(Program)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'program', 'created_at']
+    list_filter = ['created_at', 'program']
+    search_fields = ['title', 'review']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
