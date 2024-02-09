f162ddab0293c2e3ac0ae75dc2b0d5615d79221b
Ruslan Karimov <ru-ka-@mail.ru>
2024-02-09 12:19:35 +0500

    settings: fixed an exit bug

--- base/settings.py
+++ base/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'courses.apps.CoursesConfig',
+    'students.apps.StudentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courses.apps.CoursesConfig',
-    'students.apps.StudentsConfig',
 ]
 
 MIDDLEWARE = [
